aa398053624ba23a5fbae67d6e5867037a24dc9b
Kacper Bronka <kacperbronka7@gmail.com>
2023-11-30 13:48:51 +0100

    Added pickUps

--- script.js
+++ script.js
@@ -28,6 +28,12 @@ let activeBlocks = 0;
 let started = false;
 let gameEnded = false;
 
+let ballSpeedMult = 1;
+let blockSizeMult = 1;
+let blockSpeedMult = 1;
+
+let pickUps = [];
+
 function setup() {
   system = new ParticleSystem(createVector(width / 2, 50));
 
@@ -89,6 +95,7 @@ function draw() {
       ballSpeedY *= -1;
       blocks[i][2] = false;
       points++;
+      spawnPickUp(blocks[i][0], blocks[i][1]);
       blinkClr();
       continue;
     }
@@ -96,12 +103,23 @@ function draw() {
       ballSpeedX *= -1;
       blocks[i][2] = false;
       points++;
+      spawnPickUp(blocks[i][0], blocks[i][1]);
       blinkClr();
       continue;
     }
     fill(colors[i % 5]);
     rect(blocks[i][0] + Math.floor(blockWidth / 2), blocks[i][1] + 15, blockWidth, 30, 1);
   }
+
+  for (let i = 0; i < pickUps.length; i++) {
+    const p = pickUps[i];
+    if (p.used) continue;
+
+    p.update();
+    p.checkCollision();
+    p.draw();
+  }
+
   fill(170);
 
   document.querySelector(".points").innerText = points;
@@ -114,12 +132,12 @@ function draw() {
   //drawing ball
   circle(ballX, ballY, 30);
   if (started) {
-    ballX += ballSpeedX;
-    ballY += ballSpeedY;
+    ballX += ballSpeedX * ballSpeedMult;
+    ballY += ballSpeedY * ballSpeedMult;
   }
 
   //checking for ball-paddle collision
-  if (ballX < rectX + 70 && ballX > rectX - 70 && ballY >= rectY - 20 && ballY < rectY) {
+  if (ballX < rectX + 70 * blockSizeMult && ballX > rectX - 70 * blockSizeMult && ballY >= rectY - 20 && ballY < rectY) {
     ballSpeedY *= -1;
     ballSpeedX += Math.round(map(Math.random(), 0, 1, -1, 1)) * Math.abs(Math.round(map(ballX - rectX, -70, 70, -1, 1))); //if the ball is close to the edge, it will change speed
   }
@@ -142,14 +160,14 @@ function draw() {
 
   //user input
   if (keyIsDown(RIGHT_ARROW)) {
-    rectX = constrain(rectX + rectSpeed, 60, windowWidth - 60);
+    rectX = constrain(rectX + rectSpeed * blockSpeedMult, 60, windowWidth - 60);
   }
   if (keyIsDown(LEFT_ARROW)) {
-    rectX = constrain(rectX - rectSpeed, 60, windowWidth - 60);
+    rectX = constrain(rectX - rectSpeed * blockSpeedMult, 60, windowWidth - 60);
   }
 
   //drawing paddle
-  rect(rectX, rectY, 120, 10);
+  rect(rectX, rectY, 120 * blockSizeMult, 10);
 }
 
 const end = () => {
@@ -194,6 +212,70 @@ window.addEventListener("keydown", (e) => {
     document.location.reload(false);
   }
 });
+
+const spawnPickUp = (x, y) => {
+  if (Math.random() <= 0.7) return;
+
+  switch (Math.round(Math.random() * 2)) {
+    case 0:
+      pickUps.push(new PickUp(x, y, 0));
+      break;
+    case 1:
+      pickUps.push(new PickUp(x, y, 2));
+      break;
+    case 2:
+      pickUps.push(new PickUp(x, y, 1));
+      break;
+  }
+};
+
+class PickUp {
+  constructor(x, y, type) {
+    this.x = x;
+    this.y = y;
+    this.type = type;
+    this.used = false;
+  }
+
+  update = () => {
+    this.y += 1;
+  };
+
+  draw = () => {
+    let color;
+    switch (this.type) {
+      case 0:
+        color = "#4287f5";
+        break;
+      case 1:
+        color = "#9e42f5";
+        break;
+      case 2:
+        color = "#42f5dd";
+        break;
+    }
+    fill(color);
+    circle(this.x, this.y, 20);
+    fill(170);
+  };
+
+  checkCollision = () => {
+    if (this.x < rectX + 70 * blockSizeMult && this.x > rectX - 70 * blockSizeMult && this.y >= rectY - 20 && this.y < rectY) {
+      switch (this.type) {
+        case 0:
+          ballSpeedMult += 0.1;
+          break;
+        case 1:
+          blockSizeMult += 0.1;
+          break;
+        case 2:
+          blockSpeedMult += 0.1;
+          break;
+      }
+      this.used = true;
+    }
+  };
+}
 
 let interval = setInterval(() => {
   window.scrollTo(0, 0);
